00ade5d80295a007add379d4fae5f7fd7d54e29a
Holik55 <202110715057@mhs.ubharajaya.ac.id>
2025-07-11 23:59:27 +0700

    .

---
+++ js/register.js
@@ -0,0 +1,29 @@
+document.getElementById('registerForm').addEventListener('submit', async function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const res = await fetch('https://perpustakaan-production-f0df.up.railway.app/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert('Pendaftaran berhasil! Silakan login.');
+      window.location.href = 'login.html';
+    } else {
+      alert(data.error || 'Gagal mendaftar');
+    }
+
+  } catch (error) {
+    alert('Gagal terhubung ke server');
+    console.error(error);
+  }
+});
